868b3945ddc8160dffea257e5951cbe23df80e0f
Miguel Vargas <puntadelanza86@gmail.com>
2019-11-25 18:38:36 -0500

    Se resuelve el ejemplo de manejar arreglos y eventos

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,17 +1,18 @@
 import { updateDisplay, displayLog } from './utils';
 import { api } from './api';
-import { fromEvent } from 'rxjs';
-import { map, scan, tap, concatMap } from 'rxjs/operators';
+import { fromEvent, of, from } from 'rxjs';
+import { map, scan, tap, concatMap, mergeMap } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
-    
+
     const button = document.getElementById('btn');
 
     /** get comments on button click */
     fromEvent(button, 'click').pipe(
-        scan((acc, evt) => acc + 1, 0),            
+        scan((acc, evt) => acc + 1, 0),
         concatMap(page => api.getCommentsList(page)),
+        mergeMap(comments => from(comments)),
         map(JSON.stringify),
         tap(console.log),
     ).subscribe(displayLog);
