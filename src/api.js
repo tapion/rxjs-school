f7b143682b0047c66ea7c1df3868eea5fbeef52c
Miguel Vargas <puntadelanza86@gmail.com>
2019-11-25 19:15:26 -0500

    Se termina el ejercicio de manejo de errores

--- src/api.js
+++ src/api.js
@@ -1,24 +1,32 @@
-import { timer } from 'rxjs';
-import { mapTo } from 'rxjs/operators';
+import { timer, of, throwError } from 'rxjs';
+import { mapTo, mergeMap } from 'rxjs/operators';
 
-export class api{
-    static getComment(id){
-        return timer(Math.random()*1000).pipe(
-            mapTo({id:id, comment:`comment number ${id}`})
+export class api {
+    static getComment(id) {
+        return timer(Math.random() * 1000).pipe(
+            mergeMap(x => {
+                let error = Math.random() > 0.1 ? true : false;
+                if (error) {
+                    return throwError(new Error("Error Timeout"));
+                } else {
+                    return of({ id: id, comment: `comment number ${id}` });
+                }
+
+            })
         );
     }
 
-    static getCommentsList(page){
-        const buildCommentsList = (page) =>{
+    static getCommentsList(page) {
+        const buildCommentsList = (page) => {
             let comments = [];
-            const offset = (page-1)*10;
-            for(let i=offset; i < offset+10; i++){
-                comments.push({id:i, comment:`comment number ${i}`})
+            const offset = (page - 1) * 10;
+            for (let i = offset; i < offset + 10; i++) {
+                comments.push({ id: i, comment: `comment number ${i}` })
             }
             return comments;
         }
-        return timer(Math.random()*1000).pipe(
+        return timer(Math.random() * 1000).pipe(
             mapTo(buildCommentsList(page))
         );
-    }    
+    }
 }
--- src/sandbox.js
+++ src/sandbox.js
@@ -1,20 +1,26 @@
 import { updateDisplay, displayLog } from './utils';
 import { api } from './api';
 import { fromEvent } from 'rxjs';
-import { map, scan, tap, concatMap } from 'rxjs/operators';
+import { map, scan, tap, concatMap, catchError, retry } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
-    
+
     const button = document.getElementById('btn');
 
     /** get comments on button click */
     fromEvent(button, 'click').pipe(
-        scan((acc, evt) => acc + 1, 0),            
-        concatMap(id => api.getComment(id)),
+        scan((acc, evt) => acc + 1, 0),
+        concatMap(id => api.getComment(id).pipe(
+            retry(6)
+            /*catchError((err, obser) => {
+                console.log('Error dentro del operator - concatMap: ' + err.message);
+                return obser;
+            })*/
+        )),
         map(JSON.stringify),
         tap(console.log),
-    ).subscribe(displayLog);
+    ).subscribe(displayLog, err => console.log('Error en el subscribe: ' + err.message));
 
     /** end coding */
 }
